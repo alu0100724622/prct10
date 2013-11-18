6fcc7c77bec67e2b6ae8ced06b7d6a3022bce668
Noelia <alu0100724622@ull.edu.es>
2013-11-18 15:19:12 +0000

    añado la practica de la semana pasada (prct09)

---
+++ practica9/matriznyj/test/tc_matriz.rb
@@ -0,0 +1,46 @@
+
+require "./matriz.rb"
+require "test/unit"
+require "rspec"
+
+
+class Test_Matriz < Test::Unit::TestCase
+
+	def test_mostrar
+		
+	end
+	
+	def test_suma
+		
+	end
+	
+	
+	def test_resta
+
+	end
+	
+	
+	def test_multiplicacion
+
+	end
+
+
+	def test_comparable
+
+	end
+end
+
+
+
+class Test_Matriz_Dispersa < Test::Unit::TestCase
+
+	def test_suma
+
+	end
+	
+	
+	def test_resta
+
+	end
+
+end
